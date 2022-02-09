63e44ec07bc92c58f1c4534ba8f8239a8a276c51
Francis <92971421+francis816@users.noreply.github.com>
2022-02-08 21:02:35 -0600

    4.20*: refactor token header to middleware
    Don't return the function in middleware, bc more functions to execute in app

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -11,23 +11,23 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
+// const getTokenFrom = request => {
+//     const authorization = request.get('authorization')
+//     if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//         return authorization.substring(7)
+//     }
+//     return null
+// }
 
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
 
-    const token = getTokenFrom(request)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!decodedToken.id) {
-        return response.status(401).json({ error: 'token missing or invalid' })
-    }
+    // const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    // if (!decodedToken.id) {
+    //     return response.status(401).json({ error: 'token missing or invalid' })
+    // }
     const user = await User.findById(decodedToken.id)
 
     if (!body.title || !body.url) {
--- part4/utils/middlewares.js
+++ part4/utils/middlewares.js
@@ -19,13 +19,25 @@ const errorHandler = (error, request, response, next) => {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({ error: 'invalid token' })
     }
 
     next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        // don't return here otherwise won't execuse the rest apps e.g. routes and others
+        request.token = authorization.substring(7)
+    }
+    next()
+}
+
 module.exports = {
     requestLogger,
     unknownEndpoint,
-    errorHandler
+    errorHandler,
+    tokenExtractor
 }
